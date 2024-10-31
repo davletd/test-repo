9531f6aa4b56996d63de85fdbdd251cb42f643d6
Davlet Dzhakishev <davlet.d@gmail.com>
2024-10-31 17:11:44 +0100

    Update rg name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,25 +2,25 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "cloudgeni-resource-group"
+  default     = "cloudgeni-test-resource-group"
 }
 
 variable "app_service_name" {
   description = "Name of the App Service"
   type        = string
-  default     = "cloudgeni-appservice"
+  default     = "cloudgeni-test-appservice"
 }
 
 variable "sql_server_name" {
   description = "Name of the SQL Server"
   type        = string
-  default     = "cloudgeni-sqlserver"
+  default     = "cloudgeni-test-sqlserver"
 }
 
 variable "sql_database_name" {
   description = "Name of the SQL Database"
   type        = string
-  default     = "cloudgenidatabase"
+  default     = "cloudgeni-test-database"
 }
 
 variable "location" {
