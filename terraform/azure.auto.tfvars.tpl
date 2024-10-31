a570cd81c6d2e8632144e5618b5332d341f2c600
Davlet Dzhakishev <davlet.d@gmail.com>
2024-10-31 15:55:40 +0100

    Push updates to variables

---
+++ terraform/azure.auto.tfvars.tpl
@@ -0,0 +1,4 @@
+client_id       = "${AZURE_CLIENT_ID}"
+client_secret   = "${AZURE_CLIENT_SECRET}"
+tenant_id       = "00c2299a-6609-4944-bc5a-88e811aca06e"
+sql_admin_password = "${SQL_ADMIN_PASSWORD}"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,50 +1,53 @@
+# variables.tf
 variable "resource_group_name" {
   description = "Name of the resource group"
+  type        = string
   default     = "cloudgeni-resource-group"
 }
 
 variable "app_service_name" {
   description = "Name of the App Service"
+  type        = string
   default     = "cloudgeni-appservice"
 }
 
 variable "sql_server_name" {
   description = "Name of the SQL Server"
+  type        = string
   default     = "cloudgeni-sqlserver"
 }
 
 variable "sql_database_name" {
   description = "Name of the SQL Database"
+  type        = string
   default     = "cloudgenidatabase"
 }
 
 variable "location" {
   description = "Azure location for resources"
+  type        = string
   default     = "North Europe"
 }
 
 variable "client_id" {
   description = "Azure Active Directory Application (client) ID"
   type        = string
-  default     =  "${AZURE_CLIENT_ID}"
 }
 
 variable "client_secret" {
   description = "Azure Active Directory Application (client) Secret"
   type        = string
   sensitive   = true
-  default     = "${AZURE_CLIENT_SECRET}"
 }
 
 variable "tenant_id" {
   description = "Azure Active Directory Tenant ID"
   type        = string
-  default     = "00c2299a-6609-4944-bc5a-88e811aca06e" #"8768eef3-ca99-471c-956b-7d1ab5a4da8a"  # Replace with your actual Tenant ID
+  default     = "00c2299a-6609-4944-bc5a-88e811aca06e"
 }
 
 variable "sql_admin_password" {
   description = "Administrator password for the SQL Server"
   type        = string
   sensitive   = true
-  default     = "EFoYPr!Kw27qMa3Y-qNp37HC"  # Replace with a secure password
 }
